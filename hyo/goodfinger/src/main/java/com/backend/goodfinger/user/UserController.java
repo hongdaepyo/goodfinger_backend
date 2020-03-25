8d9ef897c51ed4386516d32e799b2957b16fa89c
hyojii00 <hyovel00@gmail.com>
2020-03-26 00:27:52 +0900

    add user basic control  - add signUp  - add getUser  - add deleteUser

--- hyo/goodfinger/src/main/java/com/backend/goodfinger/user/UserController.java
+++ hyo/goodfinger/src/main/java/com/backend/goodfinger/user/UserController.java
@@ -3,32 +3,71 @@ package com.backend.goodfinger.user;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@CrossOrigin
+@RequestMapping("/users")
 public class UserController {
     private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
-    DDDD dddd;
+    UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    UserRepository userRepository;
 
-    @CrossOrigin(origins = "http://localhost:3000")
-    @GetMapping("/test")
-    public String restTest() {
-        System.out.println("dddd");
-        return "hello";
+    @PostMapping("/signUp")
+    public ResponseEntity<?> signUp(@RequestBody User user) throws Exception {
+        logger.debug("signUp started.");
+
+        userRepository.insert(user);
+
+        logger.debug("signUp ended.");
+
+        return new ResponseEntity<>("user added successfully.", HttpStatus.OK);
     }
 
-    @GetMapping("/connectDB")
-    public String connectDB() throws Exception {
-        logger.info("connectDB started.");
-
-        dddd.find();
-
-        logger.info("connectDB started.");
-        return "loglog";
+    @PostMapping("/signIn")
+    public ResponseEntity<?> signIn(@RequestBody User user) throws Exception {
+        //TODO jwt 만들어서 넣어줘
+        //TODO 암호화해야하는데 springsecurity 공부해
+        return null;
     }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<?> getUser(@PathVariable String email) throws Exception {
+        logger.debug("getUser started.");
+
+        User user = userRepository.findUserByEmail(email);
+
+        if (user == null) {
+            return new ResponseEntity<>("user is not exist.", HttpStatus.NOT_FOUND);
+        }
+
+        logger.debug("getUser ended.");
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<?> deleteUser(@PathVariable String email) throws Exception {
+        //TODO 1.pathvariable , object 매핑
+        //TODO 2.다른 delete 방식
+        //둘중하나 찾아서 ㄱ
+        logger.debug("deleteUser started.");
+
+        User user = new User();
+        user.setEmail(email);
+
+        userRepository.delete(user);
+
+        logger.debug("deleteUser ended.");
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
 }
