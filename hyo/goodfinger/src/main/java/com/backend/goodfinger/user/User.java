465f17aa3a34d9c8a3e85cf6a51ba54b777e2f12
hyojii00 <hyovel00@gmail.com>
2020-03-26 00:27:52 +0900

    add user basic control  - add signUp  - add getUser  - add deleteUser

--- hyo/goodfinger/src/main/java/com/backend/goodfinger/user/User.java
+++ hyo/goodfinger/src/main/java/com/backend/goodfinger/user/User.java
@@ -3,17 +3,23 @@ package com.backend.goodfinger.user;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "user")
+@Document(collection = "User")
 @Getter
 @Setter
 @ToString
 public class User {
-    @Id
     private String id;
+    private String email;
+    private String password;
     private String name;
-    private int type;
-    private int sex;
+    private String birth;
+    private String sex;
+    /**
+     * true: Boss , false: applicant
+     */
+    private boolean isBoss;
+
+
 }
