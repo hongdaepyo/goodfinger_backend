ee6b0418f55b4e2572efb2576e6ae48c1bd0ee11
yy9320 <pyenkr93@gmail.com>
2020-04-09 01:14:33 +0900

    [yeony] 컨트롤러에 있던 부분 service, dao로 분리
    service, dao, controller 역할별로 소스 분리

--- yeony/goodfinger-project/src/main/java/com/goodfinger/yy/service/GoodfingerServiceImpl.java
+++ yeony/goodfinger-project/src/main/java/com/goodfinger/yy/service/GoodfingerServiceImpl.java
@@ -1,13 +1,19 @@
 package com.goodfinger.yy.service;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.goodfinger.yy.dao.GoodfingerDao;
 import com.goodfinger.yy.repository.Company;
 import com.goodfinger.yy.repository.CompanyRepository;
 
@@ -18,6 +24,9 @@ public class GoodfingerServiceImpl implements GoodfingerService{
 
 	@Autowired
 	CompanyRepository repositoryCom;
+	
+	@Autowired
+	GoodfingerDao dao;
 	
 	@Override
 	public List<Company> getCompanyAll() {
@@ -32,10 +41,41 @@ public class GoodfingerServiceImpl implements GoodfingerService{
 	}
 	
 	@Override
-	public void insertCompany(Company com) {
-		log.debug("start insertCompany.");
-		repositoryCom.save(com);
-		log.debug("end insertCompany.");
+	public Company insertCompany(JSONObject param) {
+		String comstring = param.get("comString").toString();
+		ArrayList<MultipartFile> files = (ArrayList<MultipartFile>) param.get("files");
+		String filePathRoot = param.get("filePathRoot").toString();
+		
+		Company com = new Company();
+		Company successInsert = new Company();
+		
+		try {
+			JSONParser jsonpar = new JSONParser();
+			JSONObject insertCom = (JSONObject) jsonpar.parse(comstring);
+			com.setcomid(insertCom.get("comid").toString());
+			com.setLocation(insertCom.get("location").toString());
+			com.setMastername(insertCom.get("mastername").toString());
+			com.setName(insertCom.get("name").toString());
+			
+			List<String> filePath = new ArrayList<String>();
+			for(MultipartFile file2 : files){
+				String fileName = file2.getOriginalFilename();
+				String originFilePath = filePathRoot + fileName;
+				System.out.println(originFilePath);
+				File fileData = new File(originFilePath);
+				file2.transferTo(fileData);
+				filePath.add(originFilePath);
+			}
+			com.setPicture(filePath);
+			
+			dao.addCompany(com);
+			
+		} catch ( Exception  e) {
+			e.printStackTrace();
+		} 
+		successInsert = dao.findByCompanyId(com.getcomid());
+		
+		return successInsert;
 	}
 	
 	@Override
