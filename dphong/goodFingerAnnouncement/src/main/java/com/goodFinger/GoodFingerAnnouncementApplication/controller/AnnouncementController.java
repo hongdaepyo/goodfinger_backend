45057eb708fc1a54f607ce398ae48790c0db6362
dphong <dphong@kaoni.com>
2020-06-02 11:39:27 +0900

    fix cors issue

--- dphong/goodFingerAnnouncement/src/main/java/com/goodFinger/GoodFingerAnnouncementApplication/controller/AnnouncementController.java
+++ dphong/goodFingerAnnouncement/src/main/java/com/goodFinger/GoodFingerAnnouncementApplication/controller/AnnouncementController.java
@@ -2,23 +2,18 @@ package com.goodFinger.GoodFingerAnnouncementApplication.controller;
 
 import javax.annotation.Resource;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.goodFinger.GoodFingerAnnouncementApplication.service.AnnouncementServiceImpl;
 
 @RestController
-//@RequestMapping("/announcement")
 public class AnnouncementController {
 	
 	@Resource(name = "AnnouncementService")
 	private AnnouncementServiceImpl announcementServiceImpl;
 	
-	@GetMapping("/hello")
+	@GetMapping(path = "/hello")
 	public String sayHello() {
 		return "helloWorld";
 	}
